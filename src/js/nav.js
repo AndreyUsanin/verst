e3086bc3838c52ad991531edd759fe8fa64053ab
rvwdude <rvwdude@gmail.com>
2018-09-13 09:52:29 +0300

    fix

--- src/js/nav.js
+++ src/js/nav.js
@@ -4,10 +4,13 @@
 var toggle = document.querySelector(".toggle");
 var topNav = document.querySelector(".navi--top");
 
+if (toggle) {
 
-// Listen for click event on toggle var
-toggle.addEventListener('click', function() {
-  console.log("toggle clicked");
-  // toggle class "active" on topNav var
-  topNav.classList.toggle("active");
-}, false);
+  // Listen for click event on toggle var
+  toggle.addEventListener('click', function () {
+    console.log("toggle clicked");
+    // toggle class "active" on topNav var
+    topNav.classList.toggle("active");
+  }, false);
+
+}
